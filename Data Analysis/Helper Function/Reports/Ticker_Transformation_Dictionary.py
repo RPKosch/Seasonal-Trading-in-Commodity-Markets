a0717d7aa76973df007b42a419a32391297dd05c
RPKosch <r.p.kosch@gmail.com>
2025-08-31 10:55:36 +0200

    Added Ticker to Commodity Transformation Table

---
+++ Data Analysis/Helper Function/Reports/Ticker_Transformation_Dictionary.py
@@ -0,0 +1,19 @@
+futures_dict = {
+    "CC": "Cocoa",
+    "CF": "Coffee",
+    "CO": "WTI Crude Oil",
+    "CP": "Copper High Grade",
+    "CT": "Cotton #2",
+    "GD": "Gold",
+    "HE": "Lean Hogs (Globex)",
+    "HO": "Heating Oil",
+    "LE": "Live Cattle (Globex)",
+    "NG": "Natural Gas",
+    "SU": "Sugar #11",
+    "SV": "Silver 5000 oz",
+    "ZC": "Corn (Globex)",
+    "ZS": "Soybeans (Globex)",
+    "ZW": "Wheat (Globex)"
+}
+
+
